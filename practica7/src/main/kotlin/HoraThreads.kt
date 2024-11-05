2550d6ebb92296686a9873edc8523d4f454f1267
tavolop3 <tavo.lop33@gmail.com>
2024-11-05 12:49:09 -0300

    corrijo concurrencia en horathreads.kt p7

--- practica7/src/main/kotlin/HoraThreads.kt
+++ practica7/src/main/kotlin/HoraThreads.kt
@@ -5,45 +5,51 @@ import kotlin.concurrent.thread
 import kotlinx.coroutines.*
 
 fun main() = runBlocking {
-    val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
 
     // Llama a una de las funciones según el mecanismo que quieras utilizar
-    // startWithThread(dateFormat)
-    // startWithTimer(dateFormat)
-    startWithCoroutine(dateFormat)
+    // startWithThread()
+    // startWithTimer()
+    val job = launch {
+        startWithCoroutine()
+    }
+    println("desp de ejecutar la corrutina")
+    delay(10000)
+    println("cancelando desp de 10 segundos")
+    job.cancelAndJoin()
 }
 
 // Función que imprime la fecha y hora actual
-fun printCurrentDate(dateFormat: SimpleDateFormat) {
+fun printCurrentDate() {
+    val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
     val currentDate = Date()
     val formattedDate = dateFormat.format(currentDate)
     println("Hora actual: $formattedDate")
 }
 
 // Opción 1: Usar un hilo
-fun startWithThread(dateFormat: SimpleDateFormat) {
+fun startWithThread() {
     thread(start = true) {
         while (true) {
-            printCurrentDate(dateFormat)
+            printCurrentDate()
             TimeUnit.SECONDS.sleep(1)
         }
     }
 }
 
 // Opción 2: Usar Timer
-fun startWithTimer(dateFormat: SimpleDateFormat) {
+fun startWithTimer() {
     val timer = Timer()
     timer.scheduleAtFixedRate(object : TimerTask() {
         override fun run() {
-            printCurrentDate(dateFormat)
+            printCurrentDate()
         }
     }, 0, 1000)
 }
 
 // Opción 3: Usar corutinas
-suspend fun startWithCoroutine(dateFormat: SimpleDateFormat) {
+suspend fun startWithCoroutine() {
     while (true) {
-        printCurrentDate(dateFormat)
+        printCurrentDate()
         delay(1000)
     }
 }
